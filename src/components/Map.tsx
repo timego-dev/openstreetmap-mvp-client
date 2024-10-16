67c8b81b93fbb04b8f467a47c3723018d9a921fa
TimeGo Dev <timego.market@gmail.com>
2024-10-16 18:19:07 +0700

    Init project

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,32 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { useEffect } from "react";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
 
-const Map = () => {
+const RenderControl = () => {
+  const map = useMap();
+  useEffect(() => {
+    if (map) {
+      navigator.geolocation.getCurrentPosition((result) => {
+        map.flyTo(
+          {
+            lat: result.coords.latitude,
+            lng: result.coords.longitude,
+          },
+          10
+        );
+      });
+    }
+  }, [map]);
+  return (
+    <div className="fixed top-0 left-0 z-999999">
+      <input
+        type="text"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      />
+    </div>
+  );
+};
+
+const RenderMap = () => {
   return (
     <MapContainer
       className="markercluster-map"
@@ -13,8 +39,9 @@ const Map = () => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
+      <RenderControl />
     </MapContainer>
   );
 };
 
-export default Map;
+export default RenderMap;
