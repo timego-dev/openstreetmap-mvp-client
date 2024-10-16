36732f7d5f4d6488102bdca1f04513f6f82a9265
TimeGo Dev <timego.market@gmail.com>
2024-10-16 23:08:44 +0700

    Add logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,15 @@
+import LogoImage from "../assets/icon/logo.png";
+
 const Header = () => {
   return (
     <div className="fixed top-0 left-0 z-[9999] px-4 h-[80px] flex bg-white w-full gap-10 items-center">
-      <div className="text-blue-500 font-semibold text-xl">
-        OpenStreetMap MVP
+      <div className="flex items-center gap-2">
+        <img src={LogoImage} alt="" className="w-[32px] h-[32px]" />
+        <div className="text-blue-500 font-semibold text-xl ">
+          <div>OpenStreetMap MVP</div>
+        </div>
       </div>
+
       <div className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500 cursor-pointer">
         Saved Address
       </div>
